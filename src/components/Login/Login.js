f9cc233e01608bb8d0b2192de06c72615ce01d85
Laurel S <ls60@protonmail.com>
2022-01-31 22:38:31 -0800

    Adds Login form component to home page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+    const [formData, setFormData] = useState( 
+        {
+            email: '',
+            password: ''
+        }
+    )
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(formData.email, formData.password)
+    }
+
+    function handleChange(e) {
+        const {name, value} = e.target 
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
     return(
         <div className='login-wrapper'>
-            <h1>Please Log In</h1>
-            <form>
-                <label>
-                    <p>E-mail</p>
-                    <input type='text' />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type='text' />
-                </label>
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='E-mail'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                <br />
+                <input
+                    type='text'
+                    placeholder='Password'
+                    name='password'
+                    onChange={handleChange}
+                    value={formData.password}
+                />
                 <div>
-                    <button type='submit'>Submit</button>
+                    <button type='submit'>Sign in</button>
                 </div>
             </form>
         </div>
